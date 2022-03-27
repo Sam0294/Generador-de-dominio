21249e900e72acf68ff20993f8930a47531aeef8
Sam0294 <sam_f94@hotmail.com>
2022-03-27 15:46:09 +0000

    subo ejercicio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your c<ode here
+  document.getElementById("miDominio").innerHTML = generarDominio();
+};
+
+const generarDominio = () => {
+  let pronombre = ["el", "la", "los"];
+  let sustantivo = ["pez", "ballena"];
+  let adjetivo = ["grande", "pequeño", "gordo"];
+  let dominio = [".com", ".es", ".org"];
+
+  array2 = " ";
+  for (let i = 0; i < pronombre.length; i++) {
+    for (let j = 0; j < sustantivo.length; j++) {
+      for (let k = 0; k < adjetivo.length; k++) {
+        for (let l = 0; l < dominio.length; l++) {
+          array2 +=
+            pronombre[i] + sustantivo[j] + adjetivo[k] + dominio[l] + "<br>";
+        }
+      }
+    }
+  }
+  return array2;
 };
